c0db2e18e04bb8f4f1574df1062602144c99efda
hzj12345@outlook.com <q694331794>
2017-05-21 22:18:28 +0800

    test

---
+++ Thinking in Java/src/org/hzj/basic/BasicTest.java
@@ -0,0 +1,14 @@
+package org.hzj.basic;
+
+/**
+ * Created by admin on 2017/5/21.
+ */
+public class BasicTest {
+
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+
+
+    }
+
+}
