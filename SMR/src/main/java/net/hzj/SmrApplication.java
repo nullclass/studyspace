c68dccdea038d0e06cc3069f7f18020c965fdca7
hzj12345@outlook.com <q694331794>
2017-07-07 14:52:09 +0800

    文件是否编译更新

---
+++ SMR/src/main/java/net/hzj/SmrApplication.java
@@ -0,0 +1,12 @@
+package net.hzj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmrApplication.class, args);
+	}
+}
---
+++ SMR/src/test/java/net/hzj/SmrApplicationTests.java
@@ -0,0 +1,16 @@
+package net.hzj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
